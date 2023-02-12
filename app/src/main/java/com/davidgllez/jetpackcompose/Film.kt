3f026327774834c0368eec68310ff1c75bd097e5
David Gonzalez <62410785+DavidGlezQ@users.noreply.github.com>
2023-02-11 18:56:02 -0600

    section7_jc
    Classes 53 to 57

---
+++ app/src/main/java/com/davidgllez/jetpackcompose/Film.kt
@@ -0,0 +1,9 @@
+package com.davidgllez.jetpackcompose
+
+/**
+ * Created by davidgonzalez on 11/02/23
+ */
+data class Film(val name: String,
+                val description: String,
+                val score: Float,
+                val photo: String)
--- app/src/main/java/com/davidgllez/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/davidgllez/jetpackcompose/MainActivity.kt
@@ -1,14 +1,24 @@
 package com.davidgllez.jetpackcompose
 
+import FakeDatabase
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.davidgllez.jetpackcompose.ui.theme.JetpackComposeSection7Theme
 
@@ -22,22 +32,52 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    //ListBasic(FakeDatabase.getAllFilms())
+                    ListAdvance(FakeDatabase.getAllFilms())
                 }
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ListAdvance(allFilms: List<Film>) {
+    LazyColumn {
+        items(allFilms.size) {
+            //ItemListBasic(title = allFilms[it].name)
+            ItemListClick(title = allFilms[it].name, modifier = Modifier.clickable {
+
+            })
+            Divider()
+        }
+    }
 }
 
+@Composable
+fun ItemListClick(title: String, modifier: Modifier) {
+    Text(text = title, modifier = modifier
+        .fillMaxWidth()
+        .padding(dimensionResource(id = R.dimen.common_padding_default)),
+        style = MaterialTheme.typography.h6)
+}
+
+@Composable
+fun ListBasic(films: List<Film>) {
+    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+        films.forEach { ItemListBasic(title = it.name) }
+    }
+}
+
+@Composable
+fun ItemListBasic(title: String) {
+    Text(text = title, modifier = Modifier
+        .fillMaxWidth()
+        .padding(dimensionResource(id = R.dimen.common_padding_default)),
+        style = MaterialTheme.typography.h6)
+}
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetpackComposeSection7Theme {
-        Greeting("Android")
+        ListBasic(FakeDatabase.getAllFilms())
     }
 }
